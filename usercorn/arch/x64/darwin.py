7b8d574ab725a3404bdd95229ff621066f1b9c90
Ryan Hileman <lunixbochs@gmail.com>
2015-08-23 14:52:40 -0700

    bring x86_64 darwin syscalls to parity with x86

--- usercorn/arch/x64/darwin.py
+++ usercorn/arch/x64/darwin.py
@@ -2,16 +2,38 @@ from unicorn.x86_const import *
 import os
 import sys
 
+from usercorn import syscalls
+
+SYSCALLS = {
+    1: ('exit', 1),
+    2: ('fork', 0),
+    3: ('read', 3),
+    4: ('write', 3),
+    5: ('open', 3), # variable length args?
+    6: ('close', 1),
+    7: ('wait4', 4),
+    9: ('link', 2),
+    10: ('unlink', 1),
+    73: ('munmap', 2),
+    197: ('mmap', 6),
+    199: ('lseek', 3),
+}
+
 def syscall(cls):
-    regs = [X86_REG_RAX, X86_REG_RDI, X86_REG_RSI, X86_REG_RDX, X86_REG_R10, X86_REG_R8, X86_REG_R9]
-    num, a1, a2, a3, a4, a5, a6 = [cls.reg_read(r) for r in regs]
-    ret = 0
-    if num == -1:
-        pass
+    regs = [X86_REG_RDI, X86_REG_RSI, X86_REG_RDX, X86_REG_R10, X86_REG_R8, X86_REG_R9]
+    args = [cls.reg_read(r) for r in regs]
+
+    def call(name, n):
+        return getattr(syscalls, name)(cls, *args[:n]) or 0
+
+    num = cls.reg_read(X86_REG_RAX)
+    params = SYSCALLS.get(num)
+    if params:
+        ret = call(*params)
+        cls.reg_write(X86_REG_RAX, ret)
     else:
         print 'Unsupported syscall:', num
         sys.exit(1)
-    cls.reg_write(X86_REG_RAX, ret)
 
 def interrupt(cls, intno):
     if intno == 0x80:
