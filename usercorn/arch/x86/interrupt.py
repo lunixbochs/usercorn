6c9e9f21bf8c852de513904b1325f650c32ae2c8
Ryan Hileman <lunixbochs@gmail.com>
2015-08-22 18:57:33 -0700

    fix x86 32-bit linux syscalls

--- usercorn/arch/x86/interrupt.py
+++ usercorn/arch/x86/interrupt.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def syscall(cls):
-    regs = [X86_REG_EAX, X86_REG_EBX, X86_REG_ECX, X86_REG_RDX, X86_REG_ESI, X86_REG_EDI, X86_REG_EBP]
+    regs = [X86_REG_EAX, X86_REG_EBX, X86_REG_ECX, X86_REG_EDX, X86_REG_ESI, X86_REG_EDI, X86_REG_EBP]
     num, a1, a2, a3, a4, a5, a6 = [cls.reg_read(r) for r in regs]
     ret = 0
     if num == 1: # SYS_exit
