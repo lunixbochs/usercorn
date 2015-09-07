0b281fe548c3adf3e035bede5d1a3fcb3d21cb2b
Ryan Hileman <lunixbochs@gmail.com>
2015-09-06 23:45:40 -0700

    add x86 regs

--- go/arch/x86/arch.go
+++ go/arch/x86/arch.go
@@ -15,4 +15,12 @@ var Arch = &models.Arch{
 	UC_ARCH: uc.UC_ARCH_X86,
 	UC_MODE: uc.UC_MODE_32,
 	SP:      uc.UC_X86_REG_ESP,
+	Regs: map[int]string{
+		uc.UC_X86_REG_EAX: "eax",
+		uc.UC_X86_REG_EBX: "ebx",
+		uc.UC_X86_REG_ECX: "ecx",
+		uc.UC_X86_REG_EDX: "edx",
+		uc.UC_X86_REG_ESI: "esi",
+		uc.UC_X86_REG_EDI: "edi",
+	},
 }
