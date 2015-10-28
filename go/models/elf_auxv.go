e2c006fb9a6a928ab6dbd944d1837d81a03e60e3
Ryan Hileman <lunixbochs@gmail.com>
2015-10-28 16:46:11 -0700

    further improve auxv

--- go/models/elf_auxv.go
+++ go/models/elf_auxv.go
@@ -23,6 +23,8 @@ const (
 	ELF_AT_EUID
 	ELF_AT_GID
 	ELF_AT_EGID
+	ELF_AT_PLATFORM
+	ELF_AT_HWCAP
 	ELF_AT_CLKTCK       = 17
 	ELF_AT_RANDOM       = 25
 	ELF_AT_SYSINFO      = 32
@@ -42,7 +44,37 @@ func add(auxv []Elf64Auxv, t, val uint64) []Elf64Auxv {
 }
 
 func setupElfAuxv(u Usercorn) ([]Elf64Auxv, error) {
-	// calc phdr offset
+	// set up AT_RANDOM
+	var tmp [16]byte
+	if _, err := rand.Read(tmp[:]); err != nil {
+		return nil, err
+	}
+	randAddr, err := u.PushBytes(tmp[:])
+	if err != nil {
+		return nil, err
+	}
+	// insert platform string
+	platformAddr, err := u.PushBytes([]byte(u.Loader().Arch() + "\x00"))
+	if err != nil {
+		return nil, err
+	}
+	// main auxv table
+	auxv := []Elf64Auxv{
+		// TODO: set/track a page size somewhere - on Arch.OS?
+		{ELF_AT_PAGESZ, 4096},
+		{ELF_AT_BASE, u.InterpBase()},
+		{ELF_AT_FLAGS, 0},
+		{ELF_AT_ENTRY, uint64(u.BinEntry())},
+		{ELF_AT_UID, uint64(os.Getuid())},
+		{ELF_AT_EUID, uint64(os.Geteuid())},
+		{ELF_AT_GID, uint64(os.Getgid())},
+		{ELF_AT_EGID, uint64(os.Getegid())},
+		{ELF_AT_PLATFORM, platformAddr},
+		{ELF_AT_CLKTCK, 100}, // 100hz, totally fake
+		{ELF_AT_RANDOM, randAddr},
+		{ELF_AT_NULL, 0},
+	}
+	// add phdr information if present in binary
 	phdrOff, _, phdrCount := u.Loader().Header()
 	segments, _ := u.Loader().Segments()
 	for _, s := range segments {
@@ -51,34 +83,16 @@ func setupElfAuxv(u Usercorn) ([]Elf64Auxv, error) {
 			break
 		}
 	}
-
-	// set up AT_RANDOM
-	var tmp [16]byte
-	if _, err := rand.Read(tmp[:]); err != nil {
-		return nil, err
+	phdrEnt := 56
+	if u.Bits() == 32 {
+		phdrEnt = 56 // FIXME
 	}
-	var randAddr uint64
-	var err error
-	if randAddr, err = u.PushBytes(tmp[:]); err != nil {
-		return nil, err
-	}
-
-	auxv := []Elf64Auxv{
-		{ELF_AT_PHDR, u.Base()},
-		{ELF_AT_PHENT, uint64(u.Bits() * 8 * 2)},
-		{ELF_AT_PHNUM, uint64(phdrCount)},
-		// TODO: set/track a page size somewhere - on Arch.OS?
-		{ELF_AT_PAGESZ, uint64(os.Getpagesize())},
-		{ELF_AT_BASE, u.InterpBase()},
-		{ELF_AT_FLAGS, 0},
-		{ELF_AT_ENTRY, uint64(u.BinEntry())},
-		{ELF_AT_UID, uint64(os.Getuid())},
-		{ELF_AT_EUID, uint64(os.Geteuid())},
-		{ELF_AT_GID, uint64(os.Getgid())},
-		{ELF_AT_EGID, uint64(os.Getegid())},
-		{ELF_AT_CLKTCK, 100}, // 100hz, totally fake
-		{ELF_AT_RANDOM, randAddr},
-		{ELF_AT_NULL, 0},
+	if phdrOff > 0 {
+		auxv = append(auxv, []Elf64Auxv{
+			{ELF_AT_PHDR, phdrOff},
+			{ELF_AT_PHENT, uint64(phdrEnt)},
+			{ELF_AT_PHNUM, uint64(phdrCount)},
+		}...)
 	}
 	return auxv, nil
 }
