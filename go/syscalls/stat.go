b6a64e59aa5439f9bdce45be81632e93d31ed793
Ryan Hileman <lunixbochs@gmail.com>
2015-10-28 20:43:20 -0700

    tweak *stat

--- go/syscalls/stat.go
+++ go/syscalls/stat.go
@@ -14,49 +14,57 @@ func NewTargetStat(stat *syscall.Stat_t, os string, bits uint) interface{} {
 }
 
 type Timespec struct {
-	Sec  int32
-	Nsec int32
+	Sec  uint32
+	Nsec uint32
 }
 
 type Timespec64 struct {
-	Sec  int64
-	Nsec int64
+	Sec  uint64
+	Nsec uint64
 }
 
-// TODO?: this assumes LARGEFILE
+// TODO: these might only work on x86
 type LinuxStat struct {
-	Dev       uint64
-	Pad1      uint16
-	Ino       uint32 // ulong
-	Mode      uint32
-	Nlink     uint32 // ulong
-	Uid, Gid  uint32
-	Rdev      uint64
-	Pad2      uint16
-	Size      int32 // slong
-	Blksize   int32 // slong
-	Blkcnt    int32 // slong
-	Atime     Timespec
-	Mtime     Timespec
-	Ctime     Timespec
+	Dev      uint32
+	Ino      uint32
+	Mode     uint16
+	Nlink    uint16
+	Uid, Gid uint32
+	Rdev     uint32
+	Size     uint32
+	Blksize  uint32
+	Blkcnt   uint32
+
+	Atime     uint32
+	AtimeNsec uint32
+	Mtime     uint32
+	MtimeNsec uint32
+	Ctime     uint32
+	CtimeNsec uint32
+
 	Reserved4 uint32
 	Reserved5 uint32
 }
 
 type LinuxStat64 struct {
-	Dev       uint64
-	Ino       uint64 // ulong
-	Nlink     uint64 // ulong
-	Mode      uint32
-	Uid, Gid  uint32
-	Pad0      int32
-	Rdev      uint64
-	Size      int64 // slong
-	Blksize   int64 // slong
-	Blkcnt    int64 // slong
-	Atime     Timespec64
-	Mtime     Timespec64
-	Ctime     Timespec64
+	Dev      uint64
+	Ino      uint64
+	Nlink    uint64
+	Mode     uint32
+	Uid, Gid uint32
+	Pad0     uint32
+	Rdev     uint64
+	Size     int64
+	Blksize  int64
+	Blkcnt   int64
+
+	Atime     uint64
+	AtimeNsec uint64
+	Mtime     uint64
+	MtimeNsec uint64
+	Ctime     uint64
+	CtimeNsec uint64
+
 	Reserved3 [3]uint64
 }
 
