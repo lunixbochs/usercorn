a170121d0fdca450fc9dd34d5b6f31e4269b0b61
Ryan Hileman <lunixbochs@gmail.com>
2015-11-15 21:03:35 -0800

    add some syscall stubs

--- go/kernel/posix/stub.go
+++ go/kernel/posix/stub.go
@@ -5,3 +5,9 @@ func (k *PosixKernel) RtSigprocmask() {}
 func (k *PosixKernel) RtSigaction()   {}
 func (k *PosixKernel) Futex()         {}
 func (k *PosixKernel) Fcntl()         {}
+func (k *PosixKernel) SchedYield()    {}
+func (k *PosixKernel) Madvise()       {}
+func (k *PosixKernel) Mlock()         {}
+func (k *PosixKernel) Munlock()       {}
+func (k *PosixKernel) Mlockall()      {}
+func (k *PosixKernel) Munlockall()    {}
