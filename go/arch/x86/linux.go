0687980162cd100c5fc7322fd10a7f9c87686ec1
Ryan Hileman <lunixbochs@gmail.com>
2015-12-03 19:51:13 -0800

    add socketcall() for x86-linux

--- go/arch/x86/linux.go
+++ go/arch/x86/linux.go
@@ -4,22 +4,67 @@ import (
 	"github.com/lunixbochs/ghostrace/ghost/sys/num"
 	uc "github.com/unicorn-engine/unicorn/bindings/go/unicorn"
 
-	"github.com/lunixbochs/usercorn/go/kernel/common"
+	co "github.com/lunixbochs/usercorn/go/kernel/common"
 	"github.com/lunixbochs/usercorn/go/kernel/linux"
+	"github.com/lunixbochs/usercorn/go/kernel/posix"
 	"github.com/lunixbochs/usercorn/go/models"
 )
 
 var LinuxRegs = []int{uc.X86_REG_EBX, uc.X86_REG_ECX, uc.X86_REG_EDX, uc.X86_REG_ESI, uc.X86_REG_EDI, uc.X86_REG_EBP}
 
+type LinuxKernel struct {
+	linux.LinuxKernel
+}
+
+var socketCallMap = map[int]string{
+	1:  "socket",
+	2:  "bind",
+	3:  "connect",
+	4:  "listen",
+	5:  "accept",
+	6:  "getsockname",
+	7:  "getpeername",
+	8:  "socketpair",
+	9:  "send",
+	10: "recv",
+	11: "sendto",
+	12: "recvfrom",
+	13: "shutdown",
+	14: "setsockopt",
+	15: "getsockopt",
+	16: "sendmsg",
+	17: "recvmsg",
+	18: "accept4",
+}
+
+func (k *LinuxKernel) Socketcall(index int, params co.Buf) uint64 {
+	if name, ok := socketCallMap[index]; ok {
+		if sys := k.UsercornSyscall(name); sys != nil {
+			rawArgs := make([]uint32, len(sys.In))
+			if err := params.Unpack(rawArgs); err != nil {
+				return posix.UINT64_MAX
+			}
+			args := make([]uint64, len(rawArgs))
+			for i, v := range rawArgs {
+				args[i] = uint64(v)
+			}
+			return sys.Call(args)
+		}
+	}
+	return posix.UINT64_MAX // FIXME
+}
+
 func LinuxKernels(u models.Usercorn) []interface{} {
-	return []interface{}{linux.NewKernel(u)}
+	kernel := &LinuxKernel{*linux.DefaultKernel()}
+	kernel.UsercornInit(kernel, u)
+	return []interface{}{kernel}
 }
 
 func LinuxSyscall(u models.Usercorn) {
 	// TODO: handle errors or something
 	eax, _ := u.RegRead(uc.X86_REG_EAX)
 	name, _ := num.Linux_x86[int(eax)]
-	ret, _ := u.Syscall(int(eax), name, common.RegArgs(u, LinuxRegs))
+	ret, _ := u.Syscall(int(eax), name, co.RegArgs(u, LinuxRegs))
 	u.RegWrite(uc.X86_REG_EAX, ret)
 }
 
