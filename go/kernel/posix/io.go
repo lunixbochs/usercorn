da99d11a94f7a01d78b0ad0e657abfb4bb67416f
Ryan Hileman <lunixbochs@gmail.com>
2016-01-13 04:13:30 -0600

    fix problem with Pipe packing

--- go/kernel/posix/io.go
+++ go/kernel/posix/io.go
@@ -253,7 +253,11 @@ func (k *PosixKernel) Pipe(files co.Buf) uint64 {
 	var fds [2]int
 	err := syscall.Pipe(fds[:])
 	if err == nil {
-		if err := files.Pack(fds); err != nil {
+		err := files.Pack(int32(fds[0]))
+		if err == nil {
+			err = files.Pack(int32(fds[1]))
+		}
+		if err != nil {
 			return UINT64_MAX // FIXME
 		}
 	}
