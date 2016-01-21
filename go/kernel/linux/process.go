abe922513ae0238f6314f3a6a6147ffa8c258c71
Ryan Hileman <lunixbochs@gmail.com>
2016-01-21 14:07:37 -0800

    add some -32 suffixed linux syscalls

--- go/kernel/linux/process.go
+++ go/kernel/linux/process.go
@@ -4,6 +4,18 @@ func (k *LinuxKernel) Getuid32() int {
 	return k.PosixKernel.Getuid()
 }
 
+func (k *LinuxKernel) Getgid32() int {
+	return k.PosixKernel.Getgid()
+}
+
+func (k *LinuxKernel) Setgid32(gid int32) int {
+	return k.PosixKernel.Setgid(int(gid))
+}
+
+func (k *LinuxKernel) Setuid32(uid int32) int {
+	return k.PosixKernel.Setuid(int(uid))
+}
+
 func (k *LinuxKernel) Stat64() int {
 	return 0
 }
