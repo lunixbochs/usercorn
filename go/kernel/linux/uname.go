76bddc143db4c2efabfe8e9d856cb586ecacc1e3
Ryan Hileman <lunixbochs@gmail.com>
2016-02-03 22:09:57 +0100

    set linux uname field size to 65

--- go/kernel/linux/uname.go
+++ go/kernel/linux/uname.go
@@ -9,6 +9,6 @@ import (
 func (k *LinuxKernel) Uname(buf co.Buf) {
 	uname := &models.Uname{"Linux", "usercorn", "3.13.0-24-generic", "normal copy of Linux minding my business", k.U.Loader().Arch()}
 	// Pad is both OS and arch dependent? :(
-	uname.Pad(64)
+	uname.Pad(65)
 	posix.Uname(buf, uname)
 }
