8de26d6ce6d63dc28e4fdd0f812680d84f9cc5ab
Ryan Hileman <lunixbochs@gmail.com>
2016-02-08 19:01:29 -0800

    simplify file enum mappings

--- go/kernel/linux/unpack/file.go
+++ go/kernel/linux/unpack/file.go
@@ -1,39 +1,26 @@
 package unpack
 
 import (
-	"os"
+	"syscall"
 
 	"github.com/lunixbochs/usercorn/go/native/enum"
 )
 
-const (
-	O_RDONLY = 00
-	O_WRONLY = 01
-	O_RDWR   = 02
-
-	O_CREAT     = 0100
-	O_EXCL      = 0200
-	O_NOCTTY    = 0400
-	O_TRUNC     = 01000
-	O_APPEND    = 02000
-	O_NONBLOCK  = 04000
-	O_DSYNC     = 010000
-	O_SYNC      = 04010000
-	O_RSYNC     = 04010000
-	O_DIRECTORY = 0200000
-	O_NOFOLLOW  = 0400000
-	O_CLOEXEC   = 02000000
-)
-
 var fileModeMap = map[int]int{
-	O_RDONLY: os.O_RDONLY,
-	O_WRONLY: os.O_WRONLY,
-	O_RDWR:   os.O_RDWR,
-	O_APPEND: os.O_APPEND,
-	O_CREAT:  os.O_CREATE,
-	O_EXCL:   os.O_EXCL,
-	O_SYNC:   os.O_SYNC,
-	O_TRUNC:  os.O_TRUNC,
+	00:       syscall.O_RDONLY,
+	01:       syscall.O_WRONLY,
+	02:       syscall.O_RDWR,
+	0100:     syscall.O_CREAT,
+	0200:     syscall.O_EXCL,
+	0400:     syscall.O_NOCTTY,
+	01000:    syscall.O_TRUNC,
+	02000:    syscall.O_APPEND,
+	04000:    syscall.O_NONBLOCK,
+	010000:   syscall.O_DSYNC,
+	04010000: syscall.O_SYNC,
+	0200000:  syscall.O_DIRECTORY,
+	0400000:  syscall.O_NOFOLLOW,
+	02000000: syscall.O_CLOEXEC,
 }
 
 func OpenFlag(reg uint64) enum.OpenFlag {
