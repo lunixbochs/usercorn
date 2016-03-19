91b856f074897fdbdc71324bcb1bd18c63602c3c
Ryan Hileman <lunixbochs@gmail.com>
2016-03-18 17:13:39 -0700

    tweak symlink resolving

--- go/models/config.go
+++ go/models/config.go
@@ -26,36 +26,31 @@ type Config struct {
 	PrefixArgs []string
 }
 
+func (c *Config) resolveSymlink(path, target string, force bool) string {
+	link, err := os.Lstat(path)
+	if err == nil && link.Mode()&os.ModeSymlink != 0 {
+		if linked, err := os.Readlink(path); err != nil {
+			if !strings.HasPrefix(linked, "/") {
+				linked = filepath.Join(filepath.Dir(path), linked)
+				return c.PrefixPath(path, false)
+			}
+			return c.PrefixPath(linked, force)
+		}
+	}
+	exists := !os.IsNotExist(err)
+	if force || exists {
+		return target
+	}
+	return path
+}
+
 func (c *Config) PrefixPath(path string, force bool) string {
 	if c.LoadPrefix == "" {
 		return path
 	}
+	target := path
 	if filepath.IsAbs(path) {
-		target := filepath.Join(c.LoadPrefix, path)
-		link, err := os.Lstat(target)
-		if err == nil && link.Mode()&os.ModeSymlink != 0 {
-			path, err := os.Readlink(target)
-			if err == nil {
-				if !strings.HasPrefix(path, "/") {
-					path = filepath.Join(filepath.Dir(target), path)
-				}
-				return c.PrefixPath(path, force)
-			}
-		}
-		exists := !os.IsNotExist(err)
-		if force || exists {
-			return target
-		}
-	} else {
-		link, err := os.Lstat(path)
-		if err == nil && link.Mode()&os.ModeSymlink != 0 {
-			if linked, err := os.Readlink(path); err != nil {
-				if !strings.HasPrefix(linked, "/") {
-					linked = filepath.Join(filepath.Dir(path), linked)
-				}
-				return c.PrefixPath(linked, force)
-			}
-		}
+		target = filepath.Join(c.LoadPrefix, path)
 	}
-	return path
+	return c.resolveSymlink(path, target, force)
 }
