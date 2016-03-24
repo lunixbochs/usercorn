6c1d8d90c7add2325f256f2c1f739e836431b10f
Ryan Hileman <lunixbochs@gmail.com>
2016-03-24 09:12:39 -0700

    add varargs to StrucStream

--- go/models/struc_stream.go
+++ go/models/struc_stream.go
@@ -10,10 +10,22 @@ type StrucStream struct {
 	Options *struc.Options
 }
 
-func (s *StrucStream) Pack(i interface{}) error {
-	return struc.PackWithOptions(s.Stream, i, s.Options)
+func (s *StrucStream) Pack(vals ...interface{}) error {
+	for _, val := range vals {
+		err := struc.PackWithOptions(s.Stream, val, s.Options)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func (s *StrucStream) Unpack(i interface{}) error {
-	return struc.UnpackWithOptions(s.Stream, i, s.Options)
+func (s *StrucStream) Unpack(vals ...interface{}) error {
+	for _, val := range vals {
+		err := struc.UnpackWithOptions(s.Stream, val, s.Options)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
