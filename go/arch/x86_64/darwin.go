7b659da305943e2145ceefde723926b7c41d1c72
MMM <m.jahrens@web.de>
2016-10-04 17:35:44 +0000

    * started implementing commpage for darwin x86_64
    +++ only resolves to dummy data for now - reference: commpage.c/.h from apple sources

--- go/arch/x86_64/darwin.go
+++ go/arch/x86_64/darwin.go
@@ -37,6 +37,42 @@ func DarwinInit(u models.Usercorn, args, env []string) error {
 	if err := darwin.StackInit(u, args, env); err != nil {
 		return err
 	}
+	
+	//commpage
+	//TODO: move constants
+	var addr_COMM_PAGE_GTOD_GENERATION uint64
+	addr_COMM_PAGE_GTOD_GENERATION = 0x00007fffffe00000 + 0x050 + 28
+	var addr_COMM_PAGE_NT_GENERATION uint64
+	addr_COMM_PAGE_NT_GENERATION = 0x00007fffffe00000 + 0x050 + 24
+	
+	var commpageAddrBegin uint64
+	commpageAddrBegin = 0x00007fffffe00000
+	var commpageAddrEnd uint64
+	commpageAddrEnd = 0x00007fffffe01fff
+	if err := u.MemMap(commpageAddrBegin, commpageAddrEnd - commpageAddrBegin); err != nil {
+		return err
+	}
+	u.HookAdd(uc.HOOK_MEM_READ|uc.HOOK_MEM_WRITE, func(mu uc.Unicorn, access int, addr uint64, size int, value int64) {
+		if access == uc.MEM_WRITE {
+			u.Printf("\ncommpage Mem write")
+		} else {
+			u.Printf("\ncommpage Mem read")
+			if addr == addr_COMM_PAGE_GTOD_GENERATION {
+				//TODO: either write 0 in which case time lookups will fall back to syscalls
+				//or write non-zero and write current timestamp to timestamp and timestampNanosecond fields
+				one32 := []byte{1, 0, 0, 0}
+				u.MemWrite(addr_COMM_PAGE_GTOD_GENERATION, one32)
+			}
+			if addr == addr_COMM_PAGE_NT_GENERATION {
+				//TODO: either write 0 in which case time lookups will fall back to syscalls
+				//or write non-zero and write current timestamp to timestamp and timestampNanosecond fields
+				one32 := []byte{1, 0, 0, 0}
+				u.MemWrite(addr_COMM_PAGE_NT_GENERATION, one32)
+			}
+		}
+		u.Printf(": @0x%x, 0x%x = 0x%x\n", addr, size, value)
+	}, commpageAddrBegin, commpageAddrEnd)
+	
 	return AbiInit(u, DarwinSyscall)
 }
 
