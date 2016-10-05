d7fe02b829b9b971afcd808ffa3d0ff3bf134cee
MMM <m.jahrens@web.de>
2016-10-05 10:17:14 +0000

    * fixed MachVmAllocate parameters

--- go/kernel/mach/mman.go
+++ go/kernel/mach/mman.go
@@ -5,7 +5,7 @@ import (
 	"github.com/lunixbochs/usercorn/go/kernel/posix"
 )
 
-func (k *MachKernel) KernelrpcMachVmAllocateTrap(unk int, size co.Len, addrOut co.Buf) uint64 {
+func (k *MachKernel) KernelrpcMachVmAllocateTrap(target int, addrOut co.Obuf, size co.Len, flags int) uint64 {
 	mmap, err := k.U.Mmap(0, uint64(size))
 	if err != nil {
 		return posix.UINT64_MAX // FIXME
@@ -18,7 +18,7 @@ func (k *MachKernel) KernelrpcMachVmAllocateTrap(unk int, size co.Len, addrOut c
 	return 0
 }
 
-func (k *MachKernel) KernelrpcMachVmDeallocateTrap() uint64 {
+func (k *MachKernel) KernelrpcMachVmDeallocateTrap(target int, addr co.Buf, size co.Len) uint64 {
 	//TODO: implement
 	return 0
 }
