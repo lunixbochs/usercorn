388261c81fa5cb379d5a7811cb88ee04007dc519
Ryan Hileman <lunixbochs@gmail.com>
2016-10-07 14:43:19 -0700

    fix mmap behavior

--- go/kernel/posix/mman.go
+++ go/kernel/posix/mman.go
@@ -2,6 +2,7 @@ package posix
 
 import (
 	uc "github.com/unicorn-engine/unicorn/bindings/go/unicorn"
+	"io"
 	"os"
 	"syscall"
 
@@ -14,36 +15,59 @@ func (k *PosixKernel) Mmap(addrHint, size uint64, prot enum.MmapProt, flags enum
 	// TODO: how do we request an enum lookup from the current kernel?
 	MAP_FIXED := enum.MmapFlag(0x10) // on OS X and Linux anyway
 	// TODO: MAP_FIXED means abort if we can't get the address
-	var err error
-	var mmap *models.Mmap
+	var (
+		data []byte
+		path string
+		err  error
+		mmap *models.Mmap
+
+		file *os.File
+	)
+	// if there's a file descriptor, map (copy for now) the file here before messing with guest memory
+	if fd > 0 {
+		fd2, _ := syscall.Dup(int(fd))
+		if file, ok := k.Files[fd]; ok {
+			path = file.Path
+		}
+		if path != "" {
+			file = os.NewFile(uintptr(fd2), path)
+			defer file.Close()
+		}
+
+		data = make([]byte, size)
+		var pos uint64
+		for pos < size {
+			n, err := file.ReadAt(data[pos:], int64(off)+int64(pos))
+			pos += uint64(n)
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return UINT64_MAX // FIXME
+			}
+		}
+		data = data[:pos]
+	}
+	// reserve guest memory (doesn't allocate it yet)
 	if flags&MAP_FIXED != 0 {
 		mmap, err = k.U.MemReserve(addrHint, size, true)
 	} else {
 		mmap, err = k.U.MemReserve(addrHint, size, false)
 	}
+	// map and protect if allocation succeeded
 	if err == nil {
 		err = k.U.MemMapProt(mmap.Addr, mmap.Size, int(prot))
 	}
 	if err != nil {
 		return UINT64_MAX // FIXME
 	}
-	if fd > 0 {
-		path := ""
-		file, ok := k.Files[fd]
-		if ok {
-			path = file.Path
+	if fd > 0 && data != nil {
+		k.U.MemWrite(mmap.Addr, data)
+		if path != "" {
+			// register mapped files for symbolication of mapped shared libraries
+			k.U.RegisterAddr(file, mmap.Addr, size, int64(off))
 		}
-		fd2, _ := syscall.Dup(int(fd))
-		f := os.NewFile(uintptr(fd2), path)
-		// register mapped files for symbolication of mapped shared libraries
-		if ok {
-			k.U.RegisterAddr(f, mmap.Addr, size, int64(off))
-		}
-		tmp := make([]byte, size)
-		n, _ := f.ReadAt(tmp, int64(off))
-		k.U.MemWrite(mmap.Addr, tmp[:n])
-		syscall.Close(fd2)
 	}
+	// TODO: don't think we need to protect twice?
 	// currently just trusting protection enums between Unicorn/kernel to match
 	k.U.MemProtect(mmap.Addr, size, int(prot))
 	return mmap.Addr
