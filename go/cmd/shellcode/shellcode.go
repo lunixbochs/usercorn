58a5de10ff67f7ad685387b3c6a41a016844a354
Ryan Hileman <lunixbochs@gmail.com>
2017-04-30 17:14:04 -0700

    dcquals: modify ./shellcode to run x86_64 liberty

--- go/cmd/shellcode/shellcode.go
+++ go/cmd/shellcode/shellcode.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"encoding/hex"
+	"fmt"
 	"github.com/pkg/errors"
 	"io/ioutil"
 	"os"
 
 	"github.com/lunixbochs/usercorn/go/cmd"
 	"github.com/lunixbochs/usercorn/go/models"
+
+	uc "github.com/unicorn-engine/unicorn/bindings/go/unicorn"
 )
 
 func main() {
@@ -30,7 +33,7 @@ func main() {
 	}
 	c.RunUsercorn = func(args, env []string) error {
 		u := c.Usercorn
-		mem, err := u.Mmap(u.Entry(), uint64(len(shellcode)))
+		mem, err := u.Mmap(u.Entry(), 0x2000)
 		if err != nil {
 			return err
 		}
@@ -38,9 +41,33 @@ func main() {
 		if err := u.MemWrite(mem.Addr, shellcode); err != nil {
 			return err
 		}
-		u.SetEntry(mem.Addr)
+		resultBuf, err := u.Mmap(0, 0x4096)
+		if err != nil {
+			return err
+		}
+		u.HookAdd(uc.HOOK_CODE, func(_ uc.Unicorn, addr uint64, size uint32) {
+			u.Push(0)
+			u.Push(resultBuf.Addr)
+			u.Push(resultBuf.Addr) // return pointer
+		}, 0x100000, 0x100000)
+		u.HookAdd(uc.HOOK_CODE, func(_ uc.Unicorn, addr uint64, size uint32) {
+			u.Stop()
+		}, 0x102000, 0x102000)
+
+		// u.RegWrite(uc.X86_REG_RDI, resultBuf.Addr)
+
+		u.SetEntry(mem.Addr) // + 0x40)
 		u.SetExit(mem.Addr + uint64(len(shellcode)))
-		return u.Run(args, env)
+		err = u.Run(args, env)
+
+		rax, _ := u.RegRead(uc.X86_REG_RAX)
+		out, _ := u.MemRead(resultBuf.Addr, rax)
+		if err := ioutil.WriteFile("out.bin", out, 0644); err != nil {
+			fmt.Println("error writing output file", err)
+		}
+
+		fmt.Println("done!", err, rax)
+		return err
 	}
 	c.Run(os.Args, os.Environ())
 }
