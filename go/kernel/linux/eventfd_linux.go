f336f2a0b4795bc98bbc48e78f5ff27d7ad5efb6
Johannes Hessling <j.hessling@fh-muenster.de>
2017-05-03 18:30:32 +0200

    Make Tests Happy, Fix type conversion

--- go/kernel/linux/eventfd_linux.go
+++ go/kernel/linux/eventfd_linux.go
@@ -1,14 +1,13 @@
 package linux
 
 import (
-	"fmt"
 	"github.com/lunixbochs/usercorn/go/kernel/posix"
 	"syscall"
 )
 
 func (k *LinuxKernel) Eventfd2(initval, flags uint) uint64 {
 	var a3 uintptr
-	r1, _, err := syscall.Syscall(syscall.SYS_EVENTFD2, uintptr(&initval), uintptr(&flags), a3)
+	r1, _, err := syscall.Syscall(syscall.SYS_EVENTFD2, uintptr(initval), uintptr(flags), a3)
 	if err != 0 {
 		posix.Errno(err)
 	}
@@ -17,11 +16,10 @@ func (k *LinuxKernel) Eventfd2(initval, flags uint) uint64 {
 
 func (k *LinuxKernel) Eventfd(initval, flags uint) uint64 {
 	var a3 uintptr
-	r1, _, err := syscall.Syscall(syscall.SYS_EVENTFD2, uintptr(&initval), uintptr(&flags), a3)
+	r1, _, err := syscall.Syscall(syscall.SYS_EVENTFD2, uintptr(initval), uintptr(flags), a3)
 	if err != 0 {
 		posix.Errno(err)
 	}
 	return uint64(r1)
 
 }
-
