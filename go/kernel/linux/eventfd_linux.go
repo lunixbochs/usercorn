270f19b3dfe7792c292f1bf099ffd499bfc25989
Johannes Hessling <j.hessling@fh-muenster.de>
2017-05-03 19:35:23 +0200

    dont wrap error

--- go/kernel/linux/eventfd_linux.go
+++ go/kernel/linux/eventfd_linux.go
@@ -1,25 +1,25 @@
 package linux
 
 import (
-	"github.com/lunixbochs/usercorn/go/kernel/posix"
 	"syscall"
 )
 
-func (k *LinuxKernel) Eventfd2(initval, flags uint) uint64 {
+func (k *LinuxKernel) Eventfd2(initval, flags uint) (uint64, error) {
 	var a3 uintptr
-	r1, _, err := syscall.Syscall(syscall.SYS_EVENTFD2, uintptr(initval), uintptr(flags), a3)
-	if err != 0 {
-		posix.Errno(err)
+	r1, _, errno := syscall.Syscall(syscall.SYS_EVENTFD2, uintptr(initval), uintptr(flags), a3)
+	if errno != 0 {
+		return 0, errno
 	}
-	return uint64(r1)
+	return uint64(r1), nil
+
 }
 
-func (k *LinuxKernel) Eventfd(initval, flags uint) uint64 {
+func (k *LinuxKernel) Eventfd(initval, flags uint) (uint64, error) {
 	var a3 uintptr
-	r1, _, err := syscall.Syscall(syscall.SYS_EVENTFD2, uintptr(initval), uintptr(flags), a3)
-	if err != 0 {
-		posix.Errno(err)
+	r1, _, errno := syscall.Syscall(syscall.SYS_EVENTFD2, uintptr(initval), uintptr(flags), a3)
+	if errno != 0 {
+		return 0, errno
 	}
-	return uint64(r1)
+	return uint64(r1), nil
 
 }
