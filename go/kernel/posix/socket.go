e33dc893f4d261c3b9141abef3ed0f41ec2e603b
Ryan Hileman <lunixbochs@gmail.com>
2017-09-15 17:11:25 -0400

    implement poll pass-through

--- go/kernel/posix/socket.go
+++ go/kernel/posix/socket.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"os"
 	"syscall"
+	"unsafe"
 
 	co "github.com/lunixbochs/usercorn/go/kernel/common"
 	"github.com/lunixbochs/usercorn/go/native"
 )
 
 func (k *PosixKernel) Socket(domain, typ, protocol int) uint64 {
+	// TODO: enums for these
+	typ &= ^0x800 // remove SOCK_NONBLOCK for now
 	fd, err := syscall.Socket(domain, typ, protocol)
 	if err != nil {
 		return Errno(err)
@@ -119,6 +122,21 @@ func (k *PosixKernel) Select(args []co.Obuf, nfds int, readfds, writefds, errorf
 	putfdset(args[2], w)
 	putfdset(args[3], e)
 	return 0
+}
+
+func (k *PosixKernel) Poll(fds co.Buf, nfds co.Len, timeout int) uint64 {
+	tmp := make([]byte, nfds*8)
+	if err := fds.Struc().Unpack(&tmp); err != nil {
+		return UINT64_MAX // FIXME
+	}
+	n, _, errn := syscall.Syscall(syscall.SYS_POLL, uintptr(unsafe.Pointer(&tmp[0])), uintptr(nfds), uintptr(timeout))
+	if errn != 0 {
+		return Errno(errn)
+	}
+	if err := fds.Pack(&tmp); err != nil {
+		return UINT64_MAX // FIXME
+	}
+	return uint64(n)
 }
 
 func (k *PosixKernel) Socketpair(domain, typ, proto int, vector co.Obuf) uint64 {
