fc07ea06c09570355e503ee828a645f6aded14c5
Nick Aquina <nickaquina@gmail.com>
2022-08-05 17:52:09 +0200

    Do not panic on prctl call with option PR_SET_TAGGED_ADDR_CTRL

--- go/kernel/linux/prctl.go
+++ go/kernel/linux/prctl.go
@@ -1,12 +1,16 @@
 package linux
 
-import "fmt"
+import (
+	"fmt"
+	"syscall"
+)
 
 // TODO: put these somewhere. ghostrace maybe.
 const (
-	PR_SET_VMA      = 0x53564d41
-	PR_GET_DUMPABLE = 0x3
-	PR_SET_DUMPABLE = 0x4
+	PR_SET_VMA              = 0x53564d41
+	PR_GET_DUMPABLE         = 0x3
+	PR_SET_DUMPABLE         = 0x4
+	PR_SET_TAGGED_ADDR_CTRL = 55
 )
 
 func (k *LinuxKernel) Prctl(code int, arg uint64) uint64 {
@@ -23,6 +27,8 @@ func (k *LinuxKernel) Prctl(code int, arg uint64) uint64 {
 		} else {
 			return UINT64_MAX
 		}
+	case PR_SET_TAGGED_ADDR_CTRL:
+		return uint64(syscall.EINVAL)
 	}
 	panic(fmt.Sprintf("unhandled prctl code: 0x%x", code))
 }
